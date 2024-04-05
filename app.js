5447f7548da978f6c3c04428e10ae58e151201dc
matheus henrique <121587780+Matheus-silvaa@users.noreply.github.com>
2024-04-04 21:07:59 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/styles/style.css" rel="stylesheet">
+    <link href="/styles/fonts.css" rel="stylesheet">
+    <link href="/styles/media.css" rel="stylesheet">
+    <link href="/contato.html" rel="stylesheet">
+    <link href="/inicio.html" rel="stylesheet">
+    <link href="/Serviços.html" rel="stylesheet">
+    <link href="/sobre.html" rel="stylesheet">
+    <title>FAVET</title>
+</head>
+<body>
+
+<header>
+        <div id="title"> </div>
+        
+        <ul>
+            <a href="#"><li>Início</li></a>
+            <a href="#"><li>Serviços</li></a>
+            <a href="#"><li>Sobre</li></a>
+            <a href="#"><li>Contato</li></a>
+            <a href="#" id="Cadastro-se-btn"><li>Cadastro</li></a> 
+            <a href="#" id="inscreva-se-btn"><li>Login</li></a>
+        </ul>
+    </header>
+
+    <main>
+        <aside>
+            <h2><span>Inscreva-se</span></h2>
+            <h2>Na Favet</h2>
+            <p>
+                Centro comunitário, espaço para venda dos materiais reciclados, realização de eventos e muito mais.
+            </p>
+            <form>
+                <input type="text" placeholder="Nome">
+                <input type="password" placeholder="Senha">
+                <input type="submit" onclick="logar()">
+            </form>
+        </aside> 
+
+            <img src="/image.html/logo-favet.png" alt="FAVET">
+        
+    </main> 
